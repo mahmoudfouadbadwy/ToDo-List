b93e7fdae24b311744f283fede8b55bf3258ace5
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-08-12 12:33:08 -0700

    Delete an item from CoreData.

--- ToDo-List/view/ListView.swift
+++ ToDo-List/view/ListView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ListView: View {
+    @Environment(\.managedObjectContext) var context
     var items: FetchedResults<Item>?
     var body: some View {
         List {
             ForEach(items!) { item in
                 ToDoListRow(todoItem: item)
             }
+            .onDelete(perform: deleteItem(indexSet:))
         }
         .listSeparatorStyle(style: .none)
+    }
+    
+    private func deleteItem(indexSet: IndexSet) {
+        guard let items = items else {
+            return
+        }
+        for index in indexSet {
+            let item = items[index]
+            context.delete(item)
+        }
+        do {
+            try context.save()
+        } catch {
+            print("Can not delete \(error.localizedDescription)")
+        }
     }
 }
 
